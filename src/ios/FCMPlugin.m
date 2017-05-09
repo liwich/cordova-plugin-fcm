5b8b6817ae057d8d93b9f690ac28db316a25c2de
ernestohs <ernestohs@gmail.com>
2017-05-09 15:15:18 -0500

    Refactor previous solution

--- src/ios/FCMPlugin.m
+++ src/ios/FCMPlugin.m
@@ -49,26 +49,68 @@ static FCMPlugin *fcmPluginInstance;
     }];
 }
 
+/**
+ * If the app has the permission to show push notifications.
+ */
 - (void)hasPermission:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"Has permission");
-    BOOL enabled = NO;
-    UIApplication *application = [UIApplication sharedApplication];
-    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
-        enabled = application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
-    } else {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-        enabled = application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
-#pragma GCC diagnostic pop
-    }
-    
-    NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:1];
-    [message setObject:[NSNumber numberWithBool:enabled] forKey:@"isEnabled"];
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
-    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* result;
+        BOOL hasPermission = [self hasPermissionUserNotifications];
+
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                     messageAsBool:hasPermission];
+
+        [self.commandDelegate sendPluginResult:result
+                                    callbackId:command.callbackId];
+    }];
 }
 
+/**
+ * Ask for permission to show notifications.
+ *
+ * @param callback
+ *      The function to be exec as the callback
+ */
+- (void) promptForPermission:(CDVInvokedUrlCommand *)command
+{
+    if (IsAtLeastiOSVersion(@"8.0")) {
+        UIUserNotificationType types;
+        UIUserNotificationSettings *settings;
+
+        types = UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound;
+
+        settings = [UIUserNotificationSettings settingsForTypes:types
+                                                     categories:nil];
+
+        [self.commandDelegate runInBackground:^{
+            [[UIApplication sharedApplication]
+             registerUserNotificationSettings:settings];
+        }];
+    }
+}
+
+/**
+ * If the app has the permission to show push notifications.
+ */
+- (BOOL) hasPermissionUserNotifications
+{
+  BOOL enabled = NO;
+
+  // Checking if app is running iOS 8
+  if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+    enabled = application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
+  } else {
+    // app is running on iOS7
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        enabled = application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
+    #pragma GCC diagnostic pop
+  }
+
+  return enabled;
+}
 
 // UN/SUBSCRIBE TOPIC //
 - (void) subscribeToTopic:(CDVInvokedUrlCommand *)command
